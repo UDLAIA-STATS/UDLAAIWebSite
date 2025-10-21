17c11231ef50745f098a9b25c72e69a895ced711
Jenkins CI <jenkins@ci.com>
2025-10-20 19:21:25 -0500

    Deploy desde Jenkins - 286

---
+++ _astro/Navbar.astro_astro_type_script_index_0_lang.jcoe1wIY.js
@@ -0,0 +1,1 @@
+import{a as o}from"./_astro_actions.CGSrGoxz.js";const t={HOME:"/"},n=document.getElementById("logout");n.addEventListener("click",async()=>{await o.logout(null),window.location.replace(t.HOME)});
