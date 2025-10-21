2588a74ba05251b0cf2b19f52504f4146c861eec
SebastianG03 <guamandavid11235@gmail.com>
2025-10-20 23:00:06 -0500

    Input component started

---
+++ src/components/inputs/input.tsx
@@ -0,0 +1,9 @@
+import type { Component } from "solid-js";
+
+interface Props {
+
+}
+
+const InputForm:Component<Props> = ({}: Props) => {
+    
+}
--- src/consts/styles.ts
+++ src/consts/styles.ts
@@ -4,6 +4,3 @@ export const colors = {
     black: "#000000",
     containerBg: "#D9D9D9"
 }
-
-export const outlinedButtonClass =
-  "text-[#C10230] bg-white border-[#C10230] border-solid border-2 px-4 py-2 rounded-md flex items-center justify-center cursor-pointer transition";
