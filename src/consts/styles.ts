60fde035c7c0662a88bbcf398b26bdb334eb7e7b
SebastianG03 <guamandavid11235@gmail.com>
2025-10-20 20:02:23 -0500

    Redirecciones y unificacion de codigo

--- src/consts/styles.ts
+++ src/consts/styles.ts
@@ -1,4 +1,9 @@
 export const colors = {
     red: "#C10230",
+    redDark: "#A00128",
     black: "#000000",
+    containerBg: "#D9D9D9"
 }
+
+export const outlinedButtonClass =
+  "text-[#C10230] bg-white border-[#C10230] border-solid border-2 px-4 py-2 rounded-md flex items-center justify-center cursor-pointer transition";
